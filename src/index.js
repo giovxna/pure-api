93d3678059a61bd191630b5909410c14d011a727
giovxna <gi.siqueira0202@gmail.com>
2024-07-24 22:54:25 -0300

    feat: creating endpoint /users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const http = require('http')
 
+const users = require('./mocks/users')
+
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text-html'});
-    response.end('<h1>hello</h1>');
+    console.log(`request method ${request.method} | endpoint ${request.url}`);
+    
+    if (request.url == '/users' && request.method == 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html'});
+        response.end(`Cannot ${request.method} ${request.url}`);
+    }
 });
 
 server.listen(3000, () => console.log('server started on http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      id: 1,
+      name: 'giovana',
+    },
+    {
+      id: 2,
+      name: 'luan',
+    },
+    {
+      id: 3,
+      name: 'jade',
+    }
+  ];
+  
